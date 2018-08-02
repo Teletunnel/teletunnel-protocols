7e8db01952698c27501643e9cc248e176592f2b7
Maciej Krüger <mkg20001@gmail.com>
2018-08-02 11:10:24 +0200

    fix: TCP module

--- src/tcp.js
+++ src/tcp.js
@@ -1,4 +1,8 @@
+'use strict'
+
 const tcpProtos = require('./tcp-protos')
+const mafmt = require('mafmt')
+const prom = (fnc) => new Promise((resolve, reject) => fnc((err, res) => err ? reject(err) : resolve(res)))
 
 module.exports = {
   name: 'tcp',
@@ -11,7 +15,7 @@ module.exports = {
   detect: (conn) => {
     const addrs = prom(cb => conn.getObservedAddrs(cb))
     let tcp = addrs.filter(mafmt.TCP.matches)[0]
-    return tcp ? {port: tcp.toNodeOptions().port} : false
+    return tcp ? {port: tcp.nodeAddress().port} : false
   },
   stream: (conn) => conn, // well... it's already tcp
   children: tcpProtos
