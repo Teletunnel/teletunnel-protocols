90bf4ae045e889e5847e66bc1f21545e7fe0ff27
Maciej Krüger <mkg20001@gmail.com>
2018-08-04 15:41:12 +0200

    feat: SSL .stream()

--- README.md
+++ README.md
@@ -10,5 +10,8 @@ Protocol definitions and matchers for the teletunnel protocol
 
 This object can also be used to disable protos by setting the proto name to false
 
-- `ssl`:
-  - `SNICallback`: Docs [ » TLS.createServer ](https://nodejs.org/api/tls.html#tls_tls_createserver_options_secureconnectionlistener)
+## Protocols
+
+ - `ssh`: SSH v2 protocol. Does not support `/stream`
+ - `ssl`: SSL/TLS as provided by the `tls` module
+  - `connection`: SSL/TLS options as documented [here](https://nodejs.org/api/tls.html#tls_tls_createserver_options_secureconnectionlistener)
--- src/tcp-protos/ssl.js
+++ src/tcp-protos/ssl.js
@@ -1,6 +1,9 @@
 'use strict'
 
-/* const SSL = */ module.exports = {
+const toSocket = require('pull-stream-to-net-socket')
+const tls = require('tls')
+
+const SSL = module.exports = {
   name: 'ssl',
   properties: {
     hostname: {
@@ -11,7 +14,13 @@
   detect: async (conn) => {
 
   },
-  stream: async (conn) => {
-    // SSL.options.SNICallback
-  }
+  stream: (conn) => new Promise((resolve, reject) => {
+    toSocket(conn, {
+      createServer: () => tls.createServer(SSL.options.connection),
+      prefire: true
+    }, (err, socket) => {
+      if (err) return reject(err)
+      resolve(socket)
+    })
+  })
 }
